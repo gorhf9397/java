294ed15541a1097eefda7f0c9ad5e2c5c901848f
gorhf9397 <108771927+gorhf9397@users.noreply.github.com>
2022-07-13 21:17:14 +0900

    Array, for 1부터45까지 크기가 큰순,작은순 출력

---
+++ java0704/src/java0704/array2.java
@@ -0,0 +1,35 @@
+package java0704;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class array2 {
+
+	public static void main(String[] args) {
+		int[] num=new int[10];
+	    Random rand=new Random();
+	    for(int i=0;i<num.length;i++)
+	    {
+	    	num[i]=rand.nextInt(45)+1;	    	
+	    }
+
+	    for(int i=0;i<num.length;i++)
+	    {  
+	        System.out.print(num[i]+" ");
+	    }
+	    System.out.println();
+	    
+	    // java에서 제공되는 라이브러리 이용
+	    Arrays.sort(num);
+	    
+	    for(int i=0;i<num.length;i++)
+	    {  
+	        System.out.print(num[i]+" ");
+	    }
+	    System.out.println();
+	    for(int i=num.length-1; i>=0;i--) {
+	    	System.out.print(num[i]+" ");
+	    }
+	}
+
+}
